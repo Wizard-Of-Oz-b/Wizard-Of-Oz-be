ec8cdeec97a79c11b20fa2a6760ec0a7a203e904
HYUK741 <dnwlslzx12@gmail.com>
2025-09-10 16:36:26 +0900

    ✨ feat : 프로덕트 기능 추가

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -4,6 +4,7 @@ urlpatterns = [
     path("auth/", include("domains.accounts.urls")),
     path("users/", include("domains.accounts.urls")),      # 초기엔 같은 urls에 두셔도 돼요
     path("categories/", include("domains.catalog.urls_categories")),
+    path("products/", include("domains.catalog.urls_products")),
     path("products/", include("domains.catalog.urls")),
     path("reviews/", include("domains.reviews.urls")),
     path("purchases/", include("domains.orders.urls")),
