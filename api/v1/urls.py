0135b889ec6b9f1f41aa3d456b021d2fb1fb9655
ing2720 <dlguddns2@naver.com>
2025-09-11 11:33:15 +0900

    ✨ feat :user,auth,social기능구현

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,12 +1,12 @@
+# api/v1/urls.py
 from django.urls import path, include
 
 urlpatterns = [
-    path("auth/", include("domains.accounts.urls")),
-    path("users/", include("domains.accounts.urls")),      # 초기엔 같은 urls에 두셔도 돼요
-    path("categories/", include("domains.catalog.urls_categories")),
-    path("products/", include("domains.catalog.urls")),
+    path("", include("domains.accounts.urls")),          # /auth..., /users/me...
+    path("categories/", include("domains.catalog.urls")),# ← 하나로
+    path("products/",   include("domains.catalog.urls")),# ← 하나로
     path("reviews/", include("domains.reviews.urls")),
     path("purchases/", include("domains.orders.urls")),
-    path("admins/", include("domains.staff.urls")),
+    path("admins/",     include("domains.staff.urls")),
     path("admin-logs/", include("domains.staff.urls")),
 ]
