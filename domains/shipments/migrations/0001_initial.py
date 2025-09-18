7155d2994f8529134690a9b8b61ec42ba71bc7bf
ing2720 <dlguddns2@naver.com>
2025-09-18 15:05:37 +0900

    🐛 fix : 마이그레이션 의존성 깨짐 해결, 루트 권한 경고 제거, compose경고 제거

--- domains/shipments/migrations/0001_initial.py
+++ domains/shipments/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ("orders", "0002_purchase_pg_purchase_pg_tid"),
+        ("orders", "0001_initial"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
