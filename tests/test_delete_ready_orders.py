7972a247559b17c4b912dd5b94e838264c5c83bc
HYUK741 <dnwlslzx12@gmail.com>
2025-10-09 19:32:05 +0900

    - 🐛 fix : 소셜로그인 파라미터 추가

--- tests/test_delete_ready_orders.py
+++ tests/test_delete_ready_orders.py
@@ -138,8 +138,6 @@ def test_cannot_delete_paid_order(user_factory, product_factory, create_stock):
     client.force_authenticate(user=user)
 
     # PAID 상태 주문 생성
-    import uuid
-
     from domains.orders.models import Purchase
 
     order = Purchase.objects.create(
